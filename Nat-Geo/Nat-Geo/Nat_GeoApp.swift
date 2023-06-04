c90f79346897b8943c2b5bcc5ebab1016a6a14e8
mohamdan <engineerm86@gmail.com>
2023-06-04 15:38:56 +0300

    add ignore file

---
+++ Nat-Geo/Nat-Geo/Nat_GeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nat_GeoApp.swift
+//  Nat-Geo
+//
+//  Created by mohamdan on 04/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Nat_GeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
